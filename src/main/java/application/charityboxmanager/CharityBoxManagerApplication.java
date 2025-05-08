e65066508cf8f6af1fb525b1fb7ecc5ac58a9655
BigMoistLochu <krok.konrad1@gmail.com>
2025-05-08 22:29:00 +0200

    chore: initial project setup

---
+++ src/main/java/application/charityboxmanager/CharityBoxManagerApplication.java
@@ -0,0 +1,13 @@
+package application.charityboxmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityBoxManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CharityBoxManagerApplication.class, args);
+    }
+
+}
