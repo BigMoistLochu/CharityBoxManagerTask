b1f969649d639ad12aa6059d2259fad533d32736
BigMoistLochu <krok.konrad1@gmail.com>
2025-05-11 13:41:48 +0200

    feat: add global exception handler for custom exceptions

--- src/main/java/application/charityboxmanager/exception/GlobalExceptionHandler.java
+++ src/main/java/application/charityboxmanager/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package application.charityboxmanager.exception;
 
+import application.charityboxmanager.exception.exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,12 +11,44 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(CollectionBoxNotFoundException.class)
     public ResponseEntity<String> handleCollectionBoxNotFound(CollectionBoxNotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(FundraisingEventNotFoundException.class)
+    public ResponseEntity<String> handleFundraisingEventNotFound(FundraisingEventNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
     }
 
     @ExceptionHandler(InvalidCurrencyException.class)
     public ResponseEntity<String> handleInvalidCurrency(InvalidCurrencyException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(CollectionBoxNotEmptyException.class)
+    public ResponseEntity<String> handleBoxNotEmpty(CollectionBoxNotEmptyException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(CollectionBoxAlreadyAssignedException.class)
+    public ResponseEntity<String> handleBoxAlreadyAssigned(CollectionBoxAlreadyAssignedException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(FundraisingEventAlreadyAssignedException.class)
+    public ResponseEntity<String> handleEventAlreadyAssigned(FundraisingEventAlreadyAssignedException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
     }
 
 }
